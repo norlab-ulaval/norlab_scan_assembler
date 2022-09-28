01f188bea7b7a0ea34139059231c74bf10c72897
simonpierredeschenes <simon-pierre.deschenes.1@ulaval.ca>
2022-09-27 20:07:18 -0400

    It compiles on humble.

--- src/scan_assembler_node.cpp
+++ src/scan_assembler_node.cpp
@@ -1,95 +1,110 @@
-#include <ros/ros.h>
-#include <sensor_msgs/PointCloud2.h>
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
 #include <tf2_ros/transform_listener.h>
+#include <tf2_ros/buffer.h>
 #include <pointmatcher_ros/PointMatcher_ROS.h>
 #include <pointmatcher/PointMatcher.h>
-#include <geometry_msgs/TransformStamped.h>
+#include <geometry_msgs/msg/transform_stamped.hpp>
 
 typedef PointMatcher<float> PM;
 
 std::unique_ptr<tf2_ros::Buffer> tfBuffer;
-sensor_msgs::PointCloud2 firstCloud;
-sensor_msgs::PointCloud2 secondCloud;
+sensor_msgs::msg::PointCloud2 firstCloud;
+sensor_msgs::msg::PointCloud2 secondCloud;
 std::atomic_bool newFirstCloud;
 std::atomic_bool newSecondCloud;
-std::shared_ptr<PM::Transformation> transformator;
 
-void firstCloudCallback(const sensor_msgs::PointCloud2& msg)
+class ScanAssemblerNode : public rclcpp::Node
 {
-    firstCloud = msg;
-    newFirstCloud.store(true);
-}
+public:
+    ScanAssemblerNode():
+            Node("scan_assembler_node")
+    {
+    }
 
-void secondCloudCallback(const sensor_msgs::PointCloud2& msg)
-{
-    secondCloud = msg;
-    newSecondCloud.store(true);
-}
+    void firstCloudCallback(const sensor_msgs::msg::PointCloud2& msg)
+    {
+        firstCloud = msg;
+        newFirstCloud.store(true);
+    }
 
-PM::TransformationParameters findTransform(const std::string& sourceFrame, const std::string& targetFrame, const ros::Time& time, const int& transformDimension)
+    void secondCloudCallback(const sensor_msgs::msg::PointCloud2& msg)
+    {
+        secondCloud = msg;
+        newSecondCloud.store(true);
+    }
+};
+
+PM::TransformationParameters
+findTransform(const std::string& sourceFrame, const std::string& targetFrame, const rclcpp::Time& time, const int& transformDimension)
 {
-    geometry_msgs::TransformStamped tf = tfBuffer->lookupTransform(targetFrame, sourceFrame, time, ros::Duration(0.1));
+    geometry_msgs::msg::TransformStamped tf = tfBuffer->lookupTransform(targetFrame, sourceFrame, time, std::chrono::milliseconds(100));
     return PointMatcher_ROS::rosTfToPointMatcherTransformation<float>(tf, transformDimension);
 }
 
-int main(int argc, char** argv)
+int main(int argc, char**argv)
 {
-    ros::init(argc, argv, "scan_assembler_node");
-    ros::NodeHandle nodeHandle;
-    ros::NodeHandle privateNodeHandle("~");
+    rclcpp::init(argc, argv);
+
+    std::shared_ptr<ScanAssemblerNode> node = std::shared_ptr<ScanAssemblerNode>(new ScanAssemblerNode);
 
     std::string robotFrame;
-    privateNodeHandle.param<std::string>("robot_frame", robotFrame, "base_link");
+    node->declare_parameter<std::string>("robot_frame", "base_link");
+    node->get_parameter("robot_frame", robotFrame);
 
-    transformator = PM::get().TransformationRegistrar.create("RigidTransformation");
+    std::shared_ptr<PM::Transformation> transformator = PM::get().TransformationRegistrar.create("RigidTransformation");
 
-    tfBuffer = std::unique_ptr<tf2_ros::Buffer>(new tf2_ros::Buffer);
+    tfBuffer = std::unique_ptr<tf2_ros::Buffer>(new tf2_ros::Buffer(node->get_clock()));
     tf2_ros::TransformListener tfListener(*tfBuffer);
 
     newFirstCloud.store(false);
     newSecondCloud.store(false);
 
-    ros::Publisher cloudPublisher = nodeHandle.advertise<sensor_msgs::PointCloud2>("cloud_out", 1);
+    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr cloudPublisher = node->create_publisher<sensor_msgs::msg::PointCloud2>("cloud_out", 1);
 
-    ros::Subscriber firstCloudSubscriber = nodeHandle.subscribe("cloud_in_1", 1, firstCloudCallback);
-    ros::Subscriber secondCloudSubscriber = nodeHandle.subscribe("cloud_in_2", 1, secondCloudCallback);
+    rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr firstCloudSubscription =
+            node->create_subscription<sensor_msgs::msg::PointCloud2>("cloud_in_1", 1,
+                                                                     std::bind(&ScanAssemblerNode::firstCloudCallback, node, std::placeholders::_1));
+    rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr secondCloudSubscription =
+            node->create_subscription<sensor_msgs::msg::PointCloud2>("cloud_in_2", 1,
+                                                                     std::bind(&ScanAssemblerNode::secondCloudCallback, node, std::placeholders::_1));
 
-    ros::Rate loopRate(20);
-    while(ros::ok())
+    rclcpp::Rate loopRate(20);
+    while(rclcpp::ok())
     {
         if(newFirstCloud.load() && newSecondCloud.load())
         {
             newFirstCloud.store(false);
             newSecondCloud.store(false);
-            ros::Time lookupTime;
-            if(firstCloud.header.stamp < secondCloud.header.stamp)
+            rclcpp::Time earliestStamp;
+            if(rclcpp::Time(firstCloud.header.stamp) < rclcpp::Time(secondCloud.header.stamp))
             {
-                lookupTime = firstCloud.header.stamp;
+                earliestStamp = firstCloud.header.stamp;
             }
             else
             {
-                lookupTime = secondCloud.header.stamp;
+                earliestStamp = secondCloud.header.stamp;
             }
             try
             {
                 PM::TransformationParameters firstTransform = findTransform(firstCloud.header.frame_id, robotFrame,
-                                                                            lookupTime, 4);
+                                                                            firstCloud.header.stamp, 4);
                 PM::TransformationParameters secondTransform = findTransform(secondCloud.header.frame_id, robotFrame,
-                                                                             lookupTime, 4);
+                                                                             secondCloud.header.stamp, 4);
                 PM::DataPoints firstCloudPM = PointMatcher_ROS::rosMsgToPointMatcherCloud<float>(firstCloud);
                 PM::DataPoints secondCloudPM = PointMatcher_ROS::rosMsgToPointMatcherCloud<float>(secondCloud);
                 PM::DataPoints assembledCloudPM = transformator->compute(firstCloudPM, firstTransform);
                 assembledCloudPM.concatenate(transformator->compute(secondCloudPM, secondTransform));
-                sensor_msgs::PointCloud2 assembledCloud = PointMatcher_ROS::pointMatcherCloudToRosMsg<float>(assembledCloudPM, robotFrame, lookupTime);
-                cloudPublisher.publish(assembledCloud);
+                sensor_msgs::msg::PointCloud2 assembledCloud = PointMatcher_ROS::pointMatcherCloudToRosMsg<float>(assembledCloudPM, robotFrame, earliestStamp);
+                cloudPublisher->publish(assembledCloud);
             }
             catch(const tf2::TransformException& ex)
             {
-                ROS_WARN("%s", ex.what());
+                RCLCPP_WARN(node->get_logger(), "%s", ex.what());
             }
         }
 
-        ros::spinOnce();
+        rclcpp::spin_some(node);
         loopRate.sleep();
     }
 
